52ca790cff8f7abcf0db373af6149b1464048a76
kevinsmhevin <kevincparas@gmail.com>
2018-07-26 20:28:23 -0700

    added html

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+mongoose.Promise = global.Promise;
 // starts the dotenv
 require('dotenv').config();
 require('./config');
 
 const app = express();
 
+app.use(morgan('common'));
+app.use(express.json());
+
 // static files
 app.use(express.static('public'));
 
+// const { DATABASE_URL, PORT } = require('./config');
 const photoAppRouter = require('./photoAppRouter');
 
 app.use('/photos', photoAppRouter);
+
+// let server;
+
 
 app.listen((process.env.PORT || 8080), () => {
   console.log('Your app is listening');
--- test/models.test.js
+++ test/models.test.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const { TEST_DATABASE_URL } = require('../config.js');
 
 describe('database', () => {
-  it('works', () => mongoose.connect(TEST_DATABASE_URL).then(() => {
+  it('connects to the database', () => mongoose.connect(TEST_DATABASE_URL).then(() => {
     expect(true).to.equals(true);
   }));
 });
